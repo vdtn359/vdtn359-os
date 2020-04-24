84738ccbdf5ecacef7c359c7e296f46fbcd53d76
Tuan Nguyen <vdtn359@gmail.com>
2020-04-24 10:04:43 +1000

    feat(utils): add first utils package

---
+++ packages/utils/src/date/relative.ts
@@ -0,0 +1,27 @@
+import {
+	format,
+	formatDistanceStrict,
+	isAfter,
+	isBefore,
+	isEqual,
+	startOfWeek,
+} from 'date-fns';
+
+export function isBeforeOrSame(date1, date2) {
+	return isBefore(date1, date2) || isEqual(date1, date2);
+}
+
+export function isAfterOrSame(date1, date2) {
+	return isAfter(date1, date2) || isEqual(date1, date2);
+}
+
+export function weekStart(date) {
+	return startOfWeek(date, { weekStartsOn: 1 });
+}
+
+export function formatDistance(date, relativeDate) {
+	if (relativeDate.getTime() - date.getTime() >= 86400 * 1000) {
+		return format(date, 'dd/MM/yyyy hh:mm a');
+	}
+	return `${formatDistanceStrict(date, relativeDate)} ago`;
+}
