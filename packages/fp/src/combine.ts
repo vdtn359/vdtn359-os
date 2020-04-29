7a1e70da27bfac882de21617f601a74d43d56b86
Tuan Nguyen <vdtn359@gmail.com>
2020-04-29 18:49:47 +1000

    feat(fp): add functional programming package

---
+++ packages/fp/src/combine.ts
@@ -0,0 +1,17 @@
+export const pipeline = (...fns) => (...args) => {
+	let result = fns[0](...args);
+	for (const fn of fns.slice(1)) {
+		result = fn(result);
+	}
+	return result;
+};
+
+export const waterfall = (...fns) => (...args) => {
+	for (const fn of fns) {
+		fn(...args);
+	}
+};
+
+export const compose = (...fns) => pipeline(...fns.reverse());
+
+export const binary = fn => (x, y) => fn(x, y);
---
+++ packages/fp/src/factory.ts
@@ -0,0 +1,1 @@
+export const curry = fn => (fn.length === 0 ? fn() : (...pp) => curry(fn.bind(null, ...pp)));
